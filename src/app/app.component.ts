b992ab4d96d459e3e2aaed8c8285458d24afe62b
gdleao <gdleao@gmail.com>
2021-03-30 11:57:59 -0300

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,34 +7,86 @@ import { HttpClient } from "@angular/common/http";
   styleUrls: ["./app.component.css"]
 })
 export class AppComponent implements OnInit, AfterViewInit {
-  name = "AngularX";
-  systemPreference: any;
+  name = "TesteX";
+  listaOrgao: any;
+  pic: any;
 
   constructor(private http: HttpClient) {}
 
-  ngOnInit() {
-    console.log("ngOnInit");
-    //get system preference
-    this.http
-      .get("https://jsonplaceholder.typicode.com/photos")
-      .subscribe(res => {
-        //How to make it is done before go to the next life circle event?
-        this.systemPreference = res;
-      });
-    console.log("ngOnInit2");
+  async ngOnInit() {
+    console.log("(ngOnInit) ngOnInit - start");
 
-    for (let i = 0; i < 10; i++) {
-      console.log("Block statement execution no." + i);
+    console.log("(ngOnInit) call montaTela");
+    await this.montaTela();
+
+    console.log("(ngOnInit) -- print listaOrgao --");
+    if (this.listaOrgao != undefined) {
+      console.log("(ngOnInit) print listaOrgao: " + this.listaOrgao[1].albumId + " -- " + this.listaOrgao[1].title);
+    }     
+    else {
+          console.log("(ngOnInit)" + this.pic);
     }
-    console.log(this.name);
-    this.name = "zzzz";
-    console.log(this.name);
+    console.log("(ngOnInit) ++ print listaOrgao ++");
+
+
+    console.log("(ngOnInit) call preencheGetPIC");
+    await this.preencheGetPIC();
+
+    console.log("(ngOnInit) -- print pic --");
+    if (this.pic != undefined) {
+      console.log("(ngOnInit) print pic: " + this.pic[1].id + " -- " + this.pic[1].url);
+    }     
+    else {
+          console.log("(ngOnInit) " + this.pic);
+    }
+    console.log("(ngOnInit) ++ print pic ++");
+
+
+    console.log("(ngOnInit) ngOnInit - done");
   }
 
   ngAfterViewInit() {
-    console.log("ngAfterViewInit");
-    //the below got undefined, I expect the subscriber must be done at ngOnInit
-    //before reaching this function (ngAfterViewInit)
-    console.log(this.systemPreference);
+    console.log("(ngAfterViewInit) ngAfterViewInit - start");
+
+    console.log("(ngAfterViewInit) print listaOrgao/pic: " + this.pic + " / " + this.listaOrgao);
+
+    console.log("(ngAfterViewInit) ngAfterViewInit - done");
+  }
+
+  montaTela = async () => {
+    console.log("(montaTela) montaTela - start");
+
+    for (let i = 0; i < 40; i++) {
+      //console.log("(montaTela) montaTela." + i);
+    }
+
+    await this.getOrgaos().then(data => {
+      this.listaOrgao = data;
+    })
+
+    console.log("(montaTela) montaTela - done");
+  }
+
+  preencheGetPIC = async () => {
+    
+    console.log("(preencheGetPIC) preencheGetPIC - start");
+    for (let i = 0; i < 4; i++) {
+      //console.log("(preencheGetPIC) preencheGetPIC." + i);
+    }
+    
+    await this.getPIC().then(data => {
+      this.pic = data;
+    })
+
+    console.log("(preencheGetPIC) preencheGetPIC - done");
+   
+  }
+
+  async getOrgaos(): Promise<string> {
+    return await this.http.get<string>("https://jsonplaceholder.typicode.com/photos").toPromise().then();  
+  }
+
+  async getPIC(): Promise<string> {
+    return await this.http.get<string>("https://jsonplaceholder.typicode.com/photos").toPromise().then();  
   }
 }
